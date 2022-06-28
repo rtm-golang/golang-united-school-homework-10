4a84e9a922352ae889bb2472fae37bbe72d0c5ae
Rollan (rtm-golang) <rtm.golang@gmail.com>
2022-06-28 14:43:32 +0600

    Implement required handlers (name, bad, data, headers)

--- cmd/muxing/muxing.go
+++ cmd/muxing/muxing.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 	"strconv"
+	"strings"
 
 	"github.com/gorilla/mux"
 )
@@ -21,10 +23,63 @@ main function reads host/port from env just for an example, flavor it following 
 func Start(host string, port int) {
 	router := mux.NewRouter()
 
+	// Register routes (endpoints)
+	router.HandleFunc("/name/{PARAM}", HomeHandler).Methods(http.MethodGet)
+	router.HandleFunc("/bad", BadHandler).Methods(http.MethodGet)
+	router.HandleFunc("/data", DataHandler).Methods(http.MethodPost)
+	router.HandleFunc("/headers", HeaderHandler).Methods(http.MethodPost)
+
 	log.Println(fmt.Printf("Starting API server on %s:%d\n", host, port))
-	if err := http.ListenAndServe(fmt.Sprintf("%s:%d", host, port), router); err != nil {
+	if err := http.ListenAndServe(fmt.Sprintf("%s:%d", host, port), loggingMiddleware(router)); err != nil {
 		log.Fatal(err)
 	}
+}
+
+// Logging incomming requests, male lower case and remove trailing slashes
+func loggingMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		r.URL.Path = strings.TrimSuffix(r.URL.Path, "/")
+		r.URL.Path = strings.ToLower(r.URL.Path)
+		log.Println(r.Method, r.RequestURI)
+		next.ServeHTTP(w, r)
+	})
+}
+
+// Return home page
+func HomeHandler(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(fmt.Sprintf("Hello, %v!", params["PARAM"])))
+}
+
+// Return bad request
+func BadHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusInternalServerError)
+}
+
+// Return in body posted content from request body
+func DataHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		log.Println(err)
+	}
+	w.Write([]byte("I got message:\n"))
+	w.Write(body)
+}
+
+// Return in headers posted content from request headers
+func HeaderHandler(w http.ResponseWriter, r *http.Request) {
+	a, err := strconv.Atoi(r.Header.Get("a"))
+	if err != nil {
+		log.Println(err)
+	}
+	b, err := strconv.Atoi(r.Header.Get("b"))
+	if err != nil {
+		log.Println(err)
+	}
+	w.Header().Set("a+b", fmt.Sprintf("\"%v\"", a+b))
+	w.WriteHeader(http.StatusOK)
 }
 
 //main /** starts program, gets HOST:PORT param and calls Start func.
